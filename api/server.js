b2d0810ccc2ca61796e459714fe215e4f97c7ca4
unknown <lindsay.kelly15@gmail.com>
2022-03-07 17:14:52 -0500

    get and post complete

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
-const User = require('./users/model');
 const server = express();
+const User = require('./users/model');
+
+server.use(express.json());
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -9,11 +11,41 @@ server.get('/api/users', (req, res) => {
             res.json(users)
         })
         .catch(err => {
-            res.status(500).json({
-                message: "The users information could not be retrieved",
-                err: err.message
-            })
+            res.status(500).json({ message: err.message })
         })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+        })
+})
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+      User.insert(user)
+        .then(createdUser => {
+            // console.log(stuff)
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+        })
+    }
 })
 
 server.use('*', (req, res) => {
