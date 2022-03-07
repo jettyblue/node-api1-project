98fed5cc05d0d60459cbbde3ceb6bf788829219b
unknown <lindsay.kelly15@gmail.com>
2022-03-07 16:48:26 -0500

    receiving user data

--- api/server.js
+++ api/server.js
@@ -1,11 +1,23 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
 const server = express();
 
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The users information could not be retrieved",
+                err: err.message
+            })
+        })
+})
+
 server.use('*', (req, res) => {
-    res.status(404).json({
-        message: 'not found'
-    })
+    res.status(404).json({ message: 'not found' })
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
