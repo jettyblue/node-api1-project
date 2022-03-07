830200c2c0667495eddc9ff80b5672337fb5723a
unknown <lindsay.kelly15@gmail.com>
2022-03-07 16:33:04 -0500

    dependencies and scripts added

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
